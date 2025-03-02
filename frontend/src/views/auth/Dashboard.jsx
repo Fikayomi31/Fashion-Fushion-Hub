17d696e2fe98ebef8916f3aa9baf64be8f8a6627
Fikayomi31 <fikayoogundijo@gmail.com>
2025-03-02 14:00:51 -0800

    Header.jsx done with  useAuthStore to indicate whether a user is login or not

--- frontend/src/views/auth/Dashboard.jsx
+++ frontend/src/views/auth/Dashboard.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { useAuthStore } from '../../store/auth'
-import { Link } from 'react-router-dom'
+//import { useAuthStore } from '../../store/auth'
+//import { Link } from 'react-router-dom'
 
 
 export default function Dashboard() {
-    const [isLoggedIn, setIsLoading] = useAuthStore((state) => {
-        state.isLoggedIn,
-        state.user
-
-    })
+    
     
 
     return (
-    <div>
-      <h1>Home Dashboard</h1>
-    </div>
+      <div>
+        <h1>Home Dashboard</h1>
+      </div>
   )
 }
--- frontend/src/views/base/Header.jsx
+++ frontend/src/views/base/Header.jsx
@@ -1,98 +1,84 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { useAuthStore } from '../../store/auth';
-import { Link } from 'react-router-dom';
-import { CartContext } from '../../plugin/context';
-import apiInstance from '../../utils/axios';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useAuthStore } from "../../store/auth";
+import FFH_logo from '../../assets/header/FFH_logo.webp'
+import { Link } from "react-router-dom";
+
 
 
 function Header() {
-    const cartCount = useContext(CartContext)
-    const [search, setSearch] = useState("")
+    const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+    const user = useAuthStore((state) => state.user);
 
-    const [isLoggedIn, user] = useAuthStore((state) => [
-        state.isLoggedIn,
-        state.user,
-    ]);
-
-    console.log("user().vendor_id", user().vendor_id);
-
-    const navigate = useNavigate()
-
-    const handleSearchChange = (event) => {
-        setSearch(event.target.value);
-        console.log(search);
-    }
-
-    const handleSearchSubmit = () => {
-        navigate(`/search?query=${search}`)
-    }
+    
 
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">Desphixs Test</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className=" navbar-brand" href="#">
+                        <img style={{ width: 40, height: 40, borderRadius: 50 }} src={FFH_logo} alt="" />
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarSupportedContent'
+                        aria-controls="navbaSupportedContent"
+                        aria-expanded='false'
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-                            {/* <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
-                                    Pages
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle"
+                                    href="#"
+                                    id='navbarDropdown'
+                                    role="button"
+                                    data-bs-toggle='dropdown'
+                                    aria-expanded='false'
+                                >
+                                    Page
                                 </a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">About Us</a></li>
+                                <li><a className="dropdown-item" href="#">About Us</a></li>
                                     <li><a className="dropdown-item" href="#">Contact Us</a></li>
                                     <li><a className="dropdown-item" href="#">Blog </a></li>
                                     <li><a className="dropdown-item" href="#">Changelog</a></li>
                                     <li><a className="dropdown-item" href="#">Terms & Condition</a></li>
                                     <li><a className="dropdown-item" href="#">Cookie Policy</a></li>
-
                                 </ul>
-                            </li> */}
-
+                            </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
                                     Account
                                 </a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><Link to={'/customer/account/'} className="dropdown-item"><i className='fas fa-user'></i> Account</Link></li>
-                                    <li><Link className="dropdown-item" to={`/customer/orders/`}><i className='fas fa-shopping-cart'></i> Orders</Link></li>
-                                    <li><Link className="dropdown-item" to={`/customer/wishlist/`}><i className='fas fa-heart'></i> Wishlist</Link></li>
-                                    <li><Link className="dropdown-item" to={`/customer/notifications/`}><i className='fas fa-bell fa-shake'></i> Notifications</Link></li>
-                                    <li><Link className="dropdown-item" to={`/customer/settings/`}><i className='fas fa-gear fa-spin'></i> Settings</Link></li>
+                                    <li><a className="dropdown-item" href="#">Profile</a> </li>
+                                    <li><a className="dropdown-item" href="#">Orders</a> </li>
+                                    <li><a className="dropdown-item" href="#">Address</a> </li>
+                                    <li><a className="dropdown-item" href="#">Wishlist</a> </li>
+                                    <li><a className="dropdown-item" href="#">Message</a> </li>
+                                    <li><a className="dropdown-item" href="#">Notification</a> </li>
+                                    <li><a className="dropdown-item" href="#">Settings</a> </li>
                                 </ul>
                             </li>
-
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
-                                    Vendor
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><Link className="dropdown-item" to="/vendor/dashboard/"> <i className='fas fa-user'></i> Dashboard</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/products/"> <i className='bi bi-grid-fill'></i> Products</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/product/new/"> <i className='fas fa-plus-circle'></i> Add Products</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/orders/"> <i className='fas fa-shopping-cart'></i> Orders</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/earning/"> <i className='fas fa-dollar-sign'></i> Earning</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/reviews/"> <i className='fas fa-star'></i> Reviews</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/coupon/"> <i className='fas fa-tag'></i> Coupon</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/notifications/"> <i className='fas fa-bell fa-shake'></i> Notifications</Link></li>
-                                    <li><Link className="dropdown-item" to="/vendor/settings/"> <i className='fas fa-gear fa-spin'></i> Settings</Link></li>
-                                </ul>
-                            </li>
-
                         </ul>
-                        <div className="d-flex">
-                            <input onChange={handleSearchChange} name='search' className="form-control me-2" type="text" placeholder="Search" aria-label="Search" />
-                            <button onClick={handleSearchSubmit} className="btn btn-outline-success me-2" type="submit">Search</button>
-                        </div>
+                        <form className="d-flex">
+                            <input className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-success me-2" type="submit">
+                                Search
+                            </button>
+                        </form>
                         {isLoggedIn()
                             ?
                             <>
-                                <Link className="btn btn-primary me-2" to={'/customer/account/'}>Account</Link>
+                                <Link className="btn btn-primary me-2" to='/dashboard'>Dashboard</Link>
                                 <Link className="btn btn-primary me-2" to="/logout">Logout</Link>
                             </>
                             :
@@ -102,13 +88,20 @@ function Header() {
 
                             </>
                         }
-                        <Link className="btn btn-danger" to="/cart/"><i className='fas fa-shopping-cart'></i> <span id='cart-total-items'>{cartCount || 0}</span></Link>
-
+                        
+                        <button className="btn btn-danger" type="submit">
+                            <i className="fas fa-shopping-cart" />{" "}
+                            <span id="cart-total-items">4</span>
+                        </button>
                     </div>
+
+
                 </div>
+
             </nav>
         </div>
     )
+
 }
 
 export default Header
