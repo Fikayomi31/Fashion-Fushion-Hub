e1f52e3b9820faba50eaea4b37af7b098d9493c2
Fikayomi31 <fikayoogundijo@gmail.com>
2025-03-05 05:54:58 -0800

    logic for cart list in the view store folder

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,6 +18,11 @@ urlpatterns = [
     path('category/', store_views.CategoryListAPIView.as_view()),
     path('products/', store_views.ProductListAPIView.as_view()),
     path('products/<slug>', store_views.ProductDetailAPIView.as_view()),
-    path('cart/', store_views.CartAPIView.as_view())
+    path('cart/', store_views.CartAPIView.as_view()),
+    path('cart-list/<str:cart_id>/<int:user_id>/', store_views.CartListView.as_view()),
+    path('cart-list/<str:cart_id>/', store_views.CartListView.as_view()),
+    
+
+
 ]
 
